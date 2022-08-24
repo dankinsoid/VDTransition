80c31283927fdbccef522709b7c2328d0d256cae
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2022-08-24 10:22:00 +0300

    1.11.0

--- Sources/VDTransition/RelationValue.swift
+++ Sources/VDTransition/RelationValue.swift
@@ -4,6 +4,23 @@ public enum RelationValue<Value> {
 
     case absolute(Value)
     case relative(Double)
+    
+    public var absolute: Value? {
+        if case .absolute(let value) = self { return value }
+        return nil
+    }
+    
+    public var relative: Double? {
+        if case .relative(let value) = self { return value }
+        return nil
+    }
+    
+    public var type: RelationType {
+        switch self {
+        case .absolute: return .absolute
+        case .relative: return .relative
+        }
+    }
 }
 
 extension RelationValue where Value: VectorArithmetic {
@@ -18,4 +35,30 @@ extension RelationValue where Value: VectorArithmetic {
             return result
         }
     }
+}
+
+public enum RelationType: String, Hashable {
+    
+    case absolute, relative
+}
+
+extension RelationValue: Equatable where Value: Equatable {}
+extension RelationValue: Hashable where Value: Hashable {}
+
+public func / <F: BinaryFloatingPoint>(_ lhs: RelationValue<F>, _ rhs: F) -> RelationValue<F> {
+    switch lhs {
+    case .absolute(let value): return .absolute(value / rhs)
+    case .relative(let value): return .relative(value / Double(rhs))
+    }
+}
+
+public func * <F: BinaryFloatingPoint>(_ lhs: RelationValue<F>, _ rhs: F) -> RelationValue<F> {
+    switch lhs {
+    case .absolute(let value): return .absolute(value * rhs)
+    case .relative(let value): return .relative(value * Double(rhs))
+    }
+}
+
+public func * <F: BinaryFloatingPoint>(_ lhs: F, _ rhs: RelationValue<F>) -> RelationValue<F> {
+    rhs * lhs
 }
