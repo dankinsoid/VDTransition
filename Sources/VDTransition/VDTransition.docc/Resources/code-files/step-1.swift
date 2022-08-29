3e95041116eda8eb467eecf714e87cb56902c604
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2022-08-30 00:22:47 +0300

    tutorial

---
+++ Sources/VDTransition/VDTransition.docc/Resources/code-files/step-1.swift
@@ -0,0 +1,12 @@
+import UIKit
+import VDTransition
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var view1: UILabel!
+    @IBOutlet private weak var view2: UILabel!
+    @IBOutlet private weak var view3: UILabel!
+    
+    @IBAction private func replaceTap(_ sender: UIButton) {
+    }
+}
